00f06ba38a54f38612bede4f95f5913ce49519df
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-26 21:07:51 +0200

    Gestion du mmenu mobile

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,10 @@
 import './styles/app.scss';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    mobileMenuToggle.addEventListener('click', function() {
+        mobileNav.classList.toggle('active');
+    });
+});

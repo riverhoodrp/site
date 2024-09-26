1eea141a31bdbcdbe7927b104bb511fa8eabe4d7
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-26 21:05:40 +0200

    Création du controlleur pour discord

---
+++ src/Controller/DiscordController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route("/auth/discord", name: "auth_discord_")]
+final class DiscordController
+{
+    #[Route("/login", name: "login")]
+    public function login(Request $request, ClientRegistry $clientRegistry)
+    {
+    }
+
+    #[Route("/start", name: "start")]
+    public function start(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        return $clientRegistry->getClient("discord")->redirect(["identify"]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
+}

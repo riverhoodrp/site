781282ab0b6e9f17fdd428085a8e3cf11455ddba
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-27 03:41:01 +0200

    Retrait de la route de déconnexion

--- src/Controller/DiscordController.php
+++ src/Controller/DiscordController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Exception;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 #[AsController]
 #[Route("/auth/discord", name: "auth_discord_")]
-final class DiscordController
+final class DiscordController extends AbstractController
 {
     #[Route("/login", name: "login")]
     public function login(Request $request, ClientRegistry $clientRegistry)
@@ -21,10 +23,5 @@ final class DiscordController
     public function start(ClientRegistry $clientRegistry): RedirectResponse
     {
         return $clientRegistry->getClient("discord")->redirect(["identify"]);
-    }
-
-    #[Route('/logout', name: 'app_logout')]
-    public function logout()
-    {
     }
 }

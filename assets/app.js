3deafcce707c9b24a3397955e38d981180168f19
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-27 08:15:46 +0200

    Ajout de la logique du scroll

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,26 @@
 import './styles/app.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]'); // Bouton de menu mobile
-    const mobileMenu = document.querySelector('#mobile-menu'); // Menu mobile
+    const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+    const mobileMenu = document.querySelector('#mobile-menu');
 
     mobileMenuButton.addEventListener('click', function() {
-        const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true'; // Vérifie l'état actuel
+        const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
 
-        // Basculer l'attribut aria-expanded
         mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
 
-        // Basculer les classes pour afficher/masquer le menu
         mobileMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('block');
     });
 
-    const profileMenuButton = document.querySelector('#user-menu-button'); // Bouton de menu de profil
-    const profileMenu = document.querySelector('#user-menu'); // Menu de profil
+    const profileMenuButton = document.querySelector('#user-menu-button');
+    const profileMenu = document.querySelector('#user-menu');
 
     profileMenuButton.addEventListener('click', function() {
-        const isExpanded = profileMenuButton.getAttribute('aria-expanded') === 'true'; // Vérifie l'état actuel
+        const isExpanded = profileMenuButton.getAttribute('aria-expanded') === 'true';
 
-        // Basculer l'attribut aria-expanded
         profileMenuButton.setAttribute('aria-expanded', !isExpanded);
 
-        // Basculer les classes pour afficher/masquer le menu
         profileMenu.classList.toggle('hidden');
         profileMenu.classList.toggle('block');
     });
@@ -34,6 +30,36 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!profileMenuButton.contains(event.target) && !profileMenu.contains(event.target)) {
             profileMenu.classList.add('hidden');
             profileMenuButton.setAttribute('aria-expanded', 'false');
+        }
+    });
+
+    const arrow = document.querySelector('.arrow-up');
+
+    arrow.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
+
+    window.addEventListener('scroll', function() {
+        const navbar = document.querySelector('.navbar');
+        const arrow = document.querySelector('.arrow-up');
+
+        if (window.scrollY > 200) {
+            navbar.classList.add('transparent');
+            navbar.classList.add('scroll');
+            arrow.classList.add('visible');
+            arrow.classList.add('animate-jump-in');
+            arrow.classList.add('animate-once');
+            arrow.classList.add('animate-ease-in-out');
+        } else {
+            navbar.classList.remove('transparent');
+            navbar.classList.remove('scroll');
+            arrow.classList.remove('visible');
+            arrow.classList.remove('animate-jump-in');
+            arrow.classList.remove('animate-once');
+            arrow.classList.remove('animate-ease-in-out');
         }
     });
 });

69fafb34730aae22a82cb0c1aec4126858011ed7
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-27 06:04:36 +0200

    Mise à jour de la requête vers l'API pour la récupération des rôles

--- src/Security/DiscordAuthenticator.php
+++ src/Security/DiscordAuthenticator.php
@@ -16,16 +16,27 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Wohali\OAuth2\Client\Provider\DiscordResourceOwner;
 
 final class DiscordAuthenticator extends OAuth2Authenticator implements AuthenticationEntryPointInterface
 {
+    private HttpClientInterface $httpClient;
+
     public function __construct(
         private readonly ClientRegistry $clientRegistry,
         private readonly EntityManagerInterface $em,
         private readonly RouterInterface $router,
-        private readonly UserRepository $userRepository
-    ) {}
+        private readonly UserRepository $userRepository,
+        HttpClientInterface $httpClient
+    ) {
+        $this->httpClient = $httpClient;
+    }
 
     public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
     {
@@ -55,18 +66,67 @@ final class DiscordAuthenticator extends OAuth2Authenticator implements Authenti
                     $user = new User();
                     $user->setDiscordId($discordUser->getId());
                     $user->setAvatarHash($avatar);
+                    $user->setUsername($discordUser->getUsername());
                     $this->em->persist($user);
                 } else {
                     if ($user->getAvatarHash() !== $avatar) {
                         $user->setAvatarHash($avatar);
                     }
+
+                    if ($user->getUsername() !== $discordUser->getUsername()) {
+                        $user->setUsername($discordUser->getUsername());
+                    }
                 }
 
                 $this->em->flush();
 
+                dump("Token 1 " . $accessToken);
+
+                $this->updateUserRolesFromDiscord($accessToken, $user);
+
                 return $user;
             })
         );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    private function updateUserRolesFromDiscord($accessToken, User $user): void
+    {
+        $client = $this->clientRegistry->getClient("discord");
+
+        $rolesResponse = $this->httpClient->request('GET', "https://discord.com/api/users/@me/guilds/1184125456853762149/member", [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken->getToken(),
+            ],
+        ]);
+
+        $guildMember = $rolesResponse->toArray();
+
+        error_log("Guild Member Response: " . json_encode($guildMember));
+
+        $roles = $guildMember['roles'] ?? [];
+
+        $this->processUserRoles($user, $roles);
+    }
+
+    private function processUserRoles(User $user, array $roles): void
+    {
+        error_log("Processing roles: " . implode(", ", $roles));
+
+        if (in_array("1259634180615176344", $roles)) {
+            $user->addRole('ROLE_ADMIN');
+            error_log("ROLE_ADMIN added to user: " . $user->getDiscordId());
+        } else {
+            error_log("ROLE_ADMIN not added. Role ID did not match.");
+        }
+
+        $this->em->flush();
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response

657ca3e841383b8d81e1b62f99b6f3f78ffb67f1
rubenalkapov <anthony.claudon21170@gmail.com>
2024-09-28 15:00:37 +0200

    Ajout de la route du règlement dans le controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,10 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/reglement', name: 'app_reglement')]
+    public function reglement(): Response
+    {
+        return $this->render('home/reglement.html.twig');
+    }
 }

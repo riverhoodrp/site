4a738532b96e3574dce3499abd2ff6f849875dbd
rubenalkapov <anthony.claudon21170@gmail.com>
2024-10-20 14:56:23 +0200

    Ajout du nombre de joueurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,54 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/players-count', name: 'get_player_count')]
+    public function getPlayerCount(): JsonResponse
+    {
+        try {
+            $response = $this->client->request('GET', 'http://riverhood.fr:30120/riverhood_framework/players/get/count');
+            $data = $response->toArray();
+            $playerCount = $data['count'] ?? 0;
+
+            $jsonResponse = new JsonResponse([
+                'success' => true,
+                'count' => $playerCount,
+            ]);
+
+            // Ajouter les en-têtes CORS
+            $jsonResponse->headers->set('Access-Control-Allow-Origin', '*');
+            $jsonResponse->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            $jsonResponse->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+            return $jsonResponse;
+
+        } catch (TransportExceptionInterface|ClientExceptionInterface|DecodingExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface $e) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {

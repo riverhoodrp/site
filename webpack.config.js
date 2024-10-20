3144114c39cc211e56d3c2f85dd4af24e8c1de48
rubenalkapov <anthony.claudon21170@gmail.com>
2024-10-20 14:54:36 +0200

    Ajout de jQuery + Ajout d'un asset JS pour la homepage

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('home', './assets/home.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -68,7 +69,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
